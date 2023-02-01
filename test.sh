0cebd2aa0fc7c6458802e02dcc200f8f955cc7ff
FEZEU YOUMBI Emmanuel <yfezeu.emma@gmail.com>
2023-02-01 16:28:52 +0100

    TOPIC : CI/CD

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
-# npx htmlhint *.html -r style-disabled >> linthtml_report.txt;
+npx htmlhint *.html -r style-disabled >> linthtml_report.txt;
 
-# if grep "no errors" "linthtml_report.txt" >> 2;
+if grep "no errors" "linthtml_report.txt" >> 2;
 
-# then 
-#      echo 1;
-# else 
-#      echo 0;
-# fi
-
-for f in *.{html,css,js}; do npx minify "$f" > "build/$f"; done
+then 
+     echo 1;
+else 
+     echo 0;
+fi
