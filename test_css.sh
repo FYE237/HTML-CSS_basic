28e1b4df494659e82649f9ead9b412070d7efca3
FEZEU YOUMBI Emmanuel <yfezeu.emma@gmail.com>
2023-02-03 00:27:50 +0100

    TOPIC : CSS : OK

--- test_css.sh
+++ test_css.sh
@@ -1,6 +1,6 @@
-node_modules/.bin/csslint Exercice-11.css >> lintcss_report.txt;
+node_modules/.bin/csslint Exercice-11.css >> css_report.txt;
 
-if grep "error" "lintcss_report.txt" ;  
+if grep "error" "css_report.txt" ;  
 
 then 
      echo 0;
